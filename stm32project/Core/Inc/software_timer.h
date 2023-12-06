df0656d2c0b789217db027f5b0c941d5d7b3e801
yshic <tuanl799@gmail.com>
2023-12-06 12:56:07 +0700

    add software timer

--- stm32project/Core/Inc/software_timer.h
+++ stm32project/Core/Inc/software_timer.h
@@ -1,6 +1,13 @@
 #ifndef INC_SOFTWARE_TIMER_H_
 #define INC_SOFTWARE_TIMER_H_
 
+#define TICK 10
+#define MAX_TIMERS 8
 
+extern int timer_flag[MAX_TIMERS];
+
+void setTimer(int duration, int id);
+
+void timerRun();
 
 #endif /* INC_SOFTWARE_TIMER_H_ */
